6914ca3d55139984c6b0e2975fed77d8fd4e2dde
jaopredo <joao.profissional.45@gmail.com>
2024-06-10 05:29:16 -0300

    Cabeçalho, Introdução, e Apresentação do Produto

---
+++ src/fonts/index.ts
@@ -0,0 +1,3 @@
+import localFont from 'next/font/local'
+
+export const anton = localFont({ src: 'Anton-Regular.ttf' })
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,15 +4,16 @@ const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/partials/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: {
+        leaf: {
+            DEFAULT: '#25FF4F'
+        }
+      }
     },
   },
   plugins: [],
